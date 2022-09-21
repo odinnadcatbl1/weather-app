39bef7eb4fca6c9b13170f7dca5ddf6cf7bbb3a1
Salavat F <salmrproperfahr@mail.ru>
2022-09-21 10:31:37 +0300

    add store to component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useActions } from "./hooks/useActios";
+import { useTypedSelector } from "./hooks/useTypedSelector";
+
 function App() {
-    return <div className="App">hello</div>;
+    const { data, loading, error } = useTypedSelector((state) => state.weather);
+    const { fetchWeather } = useActions();
+    useEffect(() => {
+        fetchWeather("Kazan");
+    }, []);
+
+    return (
+        <div className="app">
+            <div className="container">
+                hello
+                {data.map((data) => (
+                    <div key={data.dt_txt}>{data.main.temp}</div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
